64f936e3e1d26484dc8dde16ceb843fc62018bb2
amarkosovac11 <amarkosovac11@gmail.com>
2025-10-16 21:41:39 +0200

    Implemented Navigation Bar. Added subfolders and preordered for all ui components.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
-import React from "react";
-import Navbar from "./components/Navbar";
-function App() {
-  return (
-    <div className="App">
-      <Navbar />
-    </div>
-  );
-}
+import React from 'react';
+
+import { AboutUs, Chef, FindUs, Footer, Gallery, Header, Intro, Laurels, SpecialMenu } from './container';
+import { Navbar } from './components';
+import './App.css';
+
+const App = () => (
+  <div>
+    <Navbar />
+    <Header />
+    <AboutUs />
+    <SpecialMenu />
+    <Chef />
+    <Intro />
+    <Laurels />
+    <Gallery />
+    <FindUs />
+    <Footer />
+  
+  </div>
+);
 
 export default App;
