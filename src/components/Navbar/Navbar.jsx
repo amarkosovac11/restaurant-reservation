73a332b70419c563650fe8c2acda65a8a3519f8e
amarkosovac11 <amarkosovac11@gmail.com>
2025-10-19 14:48:49 +0200

    Added some small changes on images and text.

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -10,7 +10,7 @@ const Navbar = () => {
   const [toggleMenu, setToggleMenu] = useState(false);
   return (
 
-    <nav className='app__navbar'>
+    <nav className='app__navbar' >
 
       <div className='app__navbar-logo'>
         <img src={images.gericht} alt='app logo' />
@@ -20,7 +20,7 @@ const Navbar = () => {
         <li className='p__opensans'><a href='#home'>Home</a></li>
         <li className='p__opensans'><a href='#about'>About</a></li>
         <li className='p__opensans'><a href='#menu'>Menu</a></li>
-        <li className='p__opensans'><a href='#awards'>Awards</a></li>
+        
         <li className='p__opensans'><a href='#contact'>Contact</a></li>
       </ul>
 
@@ -42,7 +42,7 @@ const Navbar = () => {
               <li className='p__opensans'><a href='#home'>Home</a></li>
               <li className='p__opensans'><a href='#about'>About</a></li>
               <li className='p__opensans'><a href='#menu'>Menu</a></li>
-              <li className='p__opensans'><a href='#awards'>Awards</a></li>
+              
               <li className='p__opensans'><a href='#contact'>Contact</a></li>
             </ul>
 
--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -7,7 +7,7 @@ import './Header.css';
 const Header = () => (
   <div className='app__header app__wrapper section__padding' id="home">
     <div className='app__wrapper_info'>
-      <SubHeading title="Chase the new flavour"/>
+      <SubHeading title="Minimum najjaci kafic u gradu"/>
       <h1 className='app__header-h1'>The Key to Fine Dining</h1>
       <p className='p__opensans' style={{margin: '2rem 0'}}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quis pharetra adipiscing ultrices vulputate posuere tristique. In sed odio nec aliquet eu proin mauris et.</p>
       <button type='button' className='custom__button'>Explore Menu</button>
